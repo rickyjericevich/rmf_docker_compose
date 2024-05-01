e530d6f3e06eac9841496baaba45834f6a75a8cb
Ricardo Jericevich <rickyjericevich@gmail.com>
2024-05-01 23:28:32 +0200

    initial commit

---
+++ rmf_web/build_rmf_web_image_locally.sh
@@ -0,0 +1,1 @@
+docker build -t local/rmf_web_dashboard_and_server .
---
+++ rmf_web/rmf_server_config.py
@@ -0,0 +1,37 @@
+# NOTE: this file is an adaptation of the default config file found in the rmf-web api-server package: https://github.com/open-rmf/rmf-web/blob/e3d2b1c460af72eda810c90e20ddfd44d04d636f/packages/api-server/api_server/default_config.py
+
+# pylint: disable=line-too-long
+config = {
+    # ip or hostname to bind the socket to, this only applies when running the server in
+    # standalone mode.
+    "host": "0.0.0.0",
+    # port to bind to, this only applies when running the server in standalone mode.
+    "port": 8000,
+    "db_url": "sqlite://:memory:",
+    # url that rmf-server is being served on.
+    # When being a proxy, this must be the url that rmf-server is mounted on.
+    # E.g. https://example.com/rmf/api/v1
+    "public_url": "http://localhost:8000",
+    "cache_directory": "run/cache",  # The directory where cached files should be stored.
+    "log_level": "DEBUG",  # https://docs.python.org/3.8/library/logging.html#levels
+    # a user that is automatically given admin privileges, note that this does not guarantee that the user exists in the identity provider.
+    "builtin_admin": "admin",
+    # path to a PEM encoded RSA public key which is used to verify JWT tokens, if the path is relative, it is based on the working dir.
+    "jwt_public_key": None,
+    # url to the oidc endpoint, used to authenticate rest requests, it should point to the well known endpoint, e.g.
+    # http://localhost:8080/auth/realms/rmf-web/.well-known/openid-configuration.
+    # NOTE: This is ONLY used for documentation purposes, the "jwt_public_key" will be the
+    # only key used to verify a token.
+    "oidc_url": None,
+    # Audience the access token is meant for. Can also be an array.
+    # Used to verify the "aud" claim.
+    "aud": "rmf_api_server",
+    # url or string that identifies the entity that issued the jwt token
+    # Used to verify the "iss" claim
+    # If iss is set to None, it means that authentication should be disabled
+    "iss": None,
+    # list of arguments passed to the ros node, "--ros-args" is automatically prepended to the list.
+    # e.g.
+    #   Run with sim time: ["-p", "use_sim_time:=true"]
+    "ros_args": [],
+}
